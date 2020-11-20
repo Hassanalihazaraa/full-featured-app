b5d528dfb1cf1cd4c10397cddb27d2993ee970f8
Hassan <hassanalihazaraa@gmail.com>
2020-11-20 17:14:10 +0100

    settings updated

--- full_featured_app/settings.py
+++ full_featured_app/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'full_featured_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'full_featured_app',
+        'USER': 'becode',
+        'PASSWORD': 'becode123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
