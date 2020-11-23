e4ce42bcff85e8d4f40e10220d089c6653ec955f
Hassan <hassanalihazaraa@gmail.com>
2020-11-23 14:39:35 +0100

    front-end stuff

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'coreyMS',
+        'title': 'Blog Post',
+        'content': 'First post content',
+        'date_posted': 'November 23, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'November 23, 2020'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('Hello world')
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('about')
+    return render(request, 'blog/about.html', {'title': 'About'})
