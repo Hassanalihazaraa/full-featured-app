18d50dd76446803283cb2b27d0997fdff00774fc
Hassan <hassanalihazaraa@gmail.com>
2020-11-24 14:06:02 +0100

    removed dummy data

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,10 @@
+from django.db.models.fields import PositiveSmallIntegerField
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'coreyMS',
-        'title': 'Blog Post',
-        'content': 'First post content',
-        'date_posted': 'November 23, 2020'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'November 23, 2020'
-    }
-]
+from .models import Post
 
 
 def home(request):
+    posts = Post.objects.all()
     context = {
         'posts': posts,
     }
