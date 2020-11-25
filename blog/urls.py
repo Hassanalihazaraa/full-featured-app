94baf47578c0c9d1dee439f65e4017b04bf15ffe
Hassan <hassanalihazaraa@gmail.com>
2020-11-25 14:59:46 +0100

    only posts owner can edit their posts by UserPassesTestMixin

--- blog/urls.py
+++ blog/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('', PostListView.as_view(), name='home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreatView.as_view(), name='post-create'),
-    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
-    path('about', views.about, name='about'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('about/', views.about, name='about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
+from django.contrib.messages.api import debug
+from django.http import request
 from django.shortcuts import render
 from .models import Post
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView,
     DetailView,
@@ -29,13 +31,20 @@ class PostCreatView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
 
 
 def about(request):
