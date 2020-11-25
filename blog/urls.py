0e495f0fbf332d3141080a1a16af6075eebff1a7
Hassan <hassanalihazaraa@gmail.com>
2020-11-25 14:08:02 +0100

    refactor home function view to class based view

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
+from .views import PostListView
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', PostListView.as_view(), name='home'),
     path('about', views.about, name='about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 
 
-def home(request):
-    posts = Post.objects.all()
-    context = {
-        'posts': posts,
-    }
-    return render(request, 'blog/home.html', context)
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
 
 def about(request):
