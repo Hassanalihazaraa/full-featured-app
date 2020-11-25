2ff9f19c11a7d0f8ca312afffa458672af6311d1
Hassan <hassanalihazaraa@gmail.com>
2020-11-25 14:20:59 +0100

    created post_detail html file to keep the django convention

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 class PostListView(ListView):
@@ -8,6 +8,11 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+    
 
 
 def about(request):
