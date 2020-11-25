111f0e03129487ba47e2c4ff1df1160221b38a3c
Hassan <hassanalihazaraa@gmail.com>
2020-11-25 17:10:38 +0100

    listed all post of the user by going to the user via url

--- blog/views.py
+++ blog/views.py
@@ -15,19 +15,19 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
+    ordering = ['-date_posted']
     paginate_by = 5
-
-    def get_query_set(self):
-        user = get_object_or_404(User, username=self.kwargs.get('username'))
-        return Post.objects.filter(author=user).order_by('-date-posted')
 
 
 class UserPostListView(ListView):
     model = Post
     template_name = 'blog/user_posts.html'
     context_object_name = 'posts'
-    ordering = ['-date_posted']
     paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
