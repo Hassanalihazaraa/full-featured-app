a74278090b4cc4226b5af5d8762d6e20b92905b9
Hassan <hassanalihazaraa@gmail.com>
2020-11-25 16:10:26 +0100

    list all posts of user by going to their profile

--- blog/urls.py
+++ blog/urls.py
@@ -5,11 +5,13 @@ from .views import (
     PostDetailView,
     PostCreatView,
     PostUpdateView,
-    PostDeleteView
+    PostDeleteView,
+    UserPostListView
 )
 
 urlpatterns = [
     path('', PostListView.as_view(), name='home'),
+    path('user/<str:username>/', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreatView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
